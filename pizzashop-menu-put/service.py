c90161e115648c38eb6bf60c8018bec4d0243817
Nguyen Ngo <nqngo@ucdavis.edu>
2017-05-12 18:55:48 -0700

    only update menu if the menu exists

--- pizzashop-menu-put/service.py
+++ pizzashop-menu-put/service.py
@@ -5,6 +5,11 @@ from botocore.exceptions import ClientError
 def handler(event, context):
   try:
     table = boto3.resource('dynamodb', region_name='us-west-1').Table('menu')
+    item = table.get_item(Key={'menu_id': event['menu_id']}).get('Item')
+    if item == None:
+      response = {}
+      response['Message'] = 'Menu does not exist. Update failed'
+      return response
     table.update_item(
       Key={'menu_id': event['menu_id']},
       UpdateExpression="SET selection = :a",
